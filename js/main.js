4e088ee63167730044cff51ecd57c685c710fa7a
Quinton Jason <quinton.jason@gmail.com>
2022-04-28 09:45:45 -0500

    added mobile nav js and css with header

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
